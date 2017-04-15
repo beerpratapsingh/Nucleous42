1014732035690a93e5d255f8a3c863a7b4b3f9bb
beerpratapsingh <beerpratap.s@pepperfry.com>
2017-04-15 18:07:36 +0530

    added range slider

---
+++ js/data-dump.js
@@ -0,0 +1,6 @@
+
+var _dbData = {
+	location: ["Mumbai", "Delhi-NCR", "Banglore", "Hyderabad", "Chennai", "Kolkata", "Kanpur", "Allahabad", "Pune"],
+	market: ["E-Commerce", "Education", "Enterprice Software", "Health Care", "Mobile", "Computer"],
+	founder: ["Anil Ambani", "Tata", "Mumbai Indians", "Reliance", "Beer Pratap Singh"]
+}
