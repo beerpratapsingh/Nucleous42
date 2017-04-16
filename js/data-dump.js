6590733b4e57d113974825bb0ffcb4a5f55e5649
beerpratapsingh <beerpratap.s@pepperfry.com>
2017-04-16 21:13:58 +0530

    Final cimmit.

--- js/data-dump.js
+++ js/data-dump.js
@@ -1,11 +1,133 @@
 
 var _dbData = {
-	location: ["Mumbai", "Delhi-NCR", "Banglore", "Hyderabad", "Chennai", "Kolkata", "Kanpur", "Allahabad", "Pune"],
+	location: ["Mumbai", "Delhi-NCR", "Bangalore", "Hyderabad", "Chennai", "Kolkata", "Kanpur", "Allahabad", "Pune"],
 	market: ["E-Commerce", "Education", "Enterprice Software", "Health Care", "Mobile", "Computer"],
 	founder: ["Anil Ambani", "Tata", "Mumbai Indians", "Reliance", "Beer Pratap Singh"],
+	// company: [
+	// 	{
+	// 		"title": "Shopsence",
+	// 		"description": "In-Store Interactive technology &",
+	// 		"logo": "images/shopsence-logo.png",
+	// 		"location": "Mumbai",
+	// 		"market": ["E-Commerce", "B2C", "Internet Of Things", "Retail"],
+	// 		"website": "www.shopsence.co",
+	// 		"employee": "50",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "Clairvolex",
+	// 		"description": "Intelluctual Property Solutions",
+	// 		"logo": "images/clairvolex-logo.png",
+	// 		"location": "Gurgaon",
+	// 		"market": ["B2B", "Legal"],
+	// 		"website": "www.clairvolex.com",
+	// 		"employee": "97",
+	// 		"stage": "Series A"
+	// 	},
+	// 	{
+	// 		"title": "Shopsence",
+	// 		"description": "In-Store Interactive technology &",
+	// 		"logo": "images/shopsence-logo.png",
+	// 		"location": "Mumbai",
+	// 		"market": ["E-Commerce", "B2C", "Internet Of Things", "Retail"],
+	// 		"website": "www.shopsence.co",
+	// 		"employee": "41",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "Ahataxis",
+	// 		"description": "Inline Taxi Aggregator for One",
+	// 		"logo": "images/ahataxis-logo.png",
+	// 		"location": "-",
+	// 		"market": ["B2C", "Transportaion", "E-Commerce", "Travel"],
+	// 		"website": "www.ahataxis.com",
+	// 		"employee": "-",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "UrbanClap",
+	// 		"description": "Mobile-firt open marketplace for",
+	// 		"logo": "images/urbanclap-logo.png",
+	// 		"location": "Delhi",
+	// 		"market": ["E-Commerce", "B2C", "Transportaion", "Travel"],
+	// 		"website": "www.urbanclap.com",
+	// 		"employee": "188",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "Pickrr",
+	// 		"description": "On Demand first mile courier",
+	// 		"logo": "images/pickrr-logo.png",
+	// 		"location": "New Delhi",
+	// 		"market": ["B2C", "B2B"],
+	// 		"website": "www.pickrr.co",
+	// 		"employee": "-",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "UrbanClap",
+	// 		"description": "Mobile-firt open marketplace for",
+	// 		"logo": "images/urbanclap-logo.png",
+	// 		"location": "Delhi",
+	// 		"market": ["E-Commerce", "B2C", "Transportaion", "Travel"],
+	// 		"website": "www.urbanclap.com",
+	// 		"employee": "188",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "Hipship",
+	// 		"description": "Online Package delivery",
+	// 		"logo": "images/shopsence-logo.png",
+	// 		"location": "Banglore",
+	// 		"market": ["Shipping", "B2B", "Transportaion", "B2C"],
+	// 		"website": "www.hipship.com",
+	// 		"employee": "4",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "Karadi Path Education",
+	// 		"description": "Language learning for primary",
+	// 		"logo": "images/shopsence-logo.png",
+	// 		"location": "Chennai",
+	// 		"market": ["Education", "B2C"],
+	// 		"website": "www.karadipath.com",
+	// 		"employee": "-",
+	// 		"stage": "Series A"
+	// 	},
+	// 	{
+	// 		"title": "Truelancer",
+	// 		"description": "Online plateform for freelancers",
+	// 		"logo": "images/shopsence-logo.png",
+	// 		"location": "New Delhi",
+	// 		"market": ["B2C", "B2B", "Human Resources", "Career", "Job Search"],
+	// 		"website": "www.truelancer.com",
+	// 		"employee": "25",
+	// 		"stage": "Pre Series A"
+	// 	},
+	// 	{
+	// 		"title": "Goodbox",
+	// 		"description": "Mobile app to chat and order online",
+	// 		"logo": "images/shopsence-logo.png",
+	// 		"location": "Banglore",
+	// 		"market": ["Local Business", "B2C", "Communication", "Telecommunication", "B2B", "E-Commerce", "Marketplace", "Retail"],
+	// 		"website": "www.goodbox.in",
+	// 		"employee": "-",
+	// 		"stage": "Series A"
+	// 	},
+	// 	{
+	// 		"title": "Hipship",
+	// 		"description": "Online Package delivery",
+	// 		"logo": "images/shopsence-logo.png",
+	// 		"location": "Chennai",
+	// 		"market": ["Travel", "B2B", "Transportaion", "B2C"],
+	// 		"website": "www.hipship.com",
+	// 		"employee": "25",
+	// 		"stage": "Series A"
+	// 	}
+	// ]
 }
 
-var newcol={
+/*var newcol={
     company:[{
     	title:'test',
     	subtitle:'subtitle -text',
@@ -19,4 +141,4 @@ var newcol={
     location:["Mumbai", "Delhi-NCR", "Banglore", "Hyderabad"],
     market: ["E-Commerce", "Education", "Enterprice Software", "Health Care", "Mobile", "Computer"],
 	founder: ["Anil Ambani", "Tata", "Mumbai Indians", "Reliance", "Beer Pratap Singh"]
-};
+};*/
--- js/script.js
+++ js/script.js
@@ -1,69 +1,123 @@
-$(function(){
-	var leftVal = 0,
-		rightVal = 100;
-	// intialise slider
-	$(".range-slider").bootstrapSlider({
-		range: true, 
-		value: [leftVal, rightVal]
-	}) 
-	.on("slide", function(slideEvt) {
-		if(slideEvt.value.length > 0){
-			$(".slide-left").val(slideEvt.value[0]);
-			$(".slide-right").val(slideEvt.value[1]);
-		}
-	});
-	
-	$('div.list-inline').slice(0,3).show();
-	$(".load-more").on('click',function(){
-		if($('div.list-inline:hidden').length>1){
-	     $('div.list-inline:hidden').slice(0,2).show();		
-		}else{
-			$(this).addClass("disabled");
-		}
-     
 
-	});
-	$(".reset-slider").on("click", function(){
-		$(".slide-left").val(leftVal);
-		$(".slide-right").val(rightVal);
-		$(".range-slider").slider('setValue', [leftVal, rightVal]); 
-	})
+var n42 = {
+	init: function(){
+		var leftVal = 0, rightVal = 100;
+		// intialise slider
+		$(".range-slider").bootstrapSlider({
+			range: true, 
+			value: [leftVal, rightVal]
+		}).on("slide", function(slideEvt) {
+			if(slideEvt.value.length > 0){
+				$(".slide-left").val(slideEvt.value[0]);
+				$(".slide-right").val(slideEvt.value[1]);
+			}
+		});
+		$(".reset-slider").on("click", function(){
+			$(".slide-left").val(leftVal);
+			$(".slide-right").val(rightVal);
+			$(".range-slider").slider('setValue', [leftVal, rightVal]); 
+		});
 
-	$( "#location" ).autocomplete({
-      	source: _dbData.location
-    });
+		//Load more companis data
+		$('div.list-inline').slice(0,5).show();
+		$(".load-more").on('click',function(){
+			if($('div.list-inline:hidden').length>1){
+		     $('div.list-inline:hidden').slice(0,2).show();
+		     if($('div.list-inline:hidden').length<1){	
+		     	$(this).addClass("disabled");
+		     }	
+			}else{
+				$(this).addClass("disabled");
+			}
+		});
 
-    $( "#searchMarket" ).autocomplete({
-      	source: _dbData.market
-    });
+		//Auto Seacrh section
+		$( "#searchlocation" ).autocomplete({
+	      	source: _dbData.location
+	    });
+	    $( "#searchMarket" ).autocomplete({
+	      	source: _dbData.market
+	    });
+	    var concatArr =_dbData.location.concat(_dbData.market);     
+	    $("#mainSearch").autocomplete({
+	    	source: concatArr
+	    });    
 
-    var concatArr =_dbData.location.concat(_dbData.market);     
-    $("#mainSearch").autocomplete({
-    	source: concatArr
-    });
+	    //Auto Search & filter data
+	    $(".ui-autocomplete-input").on('autocompletechange change', function () {
+	        /*$('#tagsname').html('You selected: ' + this.value);*/
+	        var slectedVal = this.value; 
+	    	var mainSearch=false;
+	    	if($(this).hasClass('global-search')){
+	    		mainSearch=true;	    				
+	    	}
+	    	$(".locationSrchVal li a, .marketSrchVal li a").each(function(i){
+	    		var listText = $(this).text();    		
+	    		console.log(listText +"======="+ slectedVal);
+	    		if(listText == slectedVal){    			    			    			    			
+	    			if(mainSearch){
+	    				if($('.srch-sec a#' +listText).length < 1)
+	    				$('.srch-sec').append('<a href="javascrtip:;" class="link close-lst" id="'+listText+'">' + listText + '<span class="remove">x</span>'+'</a>');
+	    				$(".ui-autocomplete-input").val('');
+	    			}
+	    			$(this).addClass('active');
+	    		}else{
+	    			//$(this).removeClass('active');
+	    		}
+	    	});
+	    });
+	    
+	    $(".locationSrchVal li  a, .marketSrchVal li a").on('click', function(){
+	    	$(this).removeClass('active');
+	    });
 
 
-    $(".customize-column").on('click', function(){
-    	$(".checkbox-cont").show();
-    });
-    $(".close-btn").on('click', function(){
-    	$(".checkbox-cont").hide();
-    });
+	    //remove slected values
+		$('.clear-all').on('click', function(){            	
+	    	$('.close-lst').remove();
+	    	$('ul>li>a').removeClass('active');
+	    	$("#mainSearch").val('');
+	    });
+	    $(document).on('click', '.remove', function(){        
+	    	var currVal = $(this).parent('a').attr('id');  	    	
+	    	$('ul>li>a:contains('+currVal+')').removeClass('active');
+	    	$(this).parent('.close-lst').remove();
+	    });
 
-    //$(".global-search").select2();
-    
-    $(".ui-autocomplete-input").keyup(function(){    	
-    	var slectedVal = $(this).val();
-    	
-    	$(".marketSrchVal li a").each(function(i){
-    		var myArr = [];
-    		//myArr.push[slectedVal];    		
-    	});
-    });
-    $.each(newcol,function(key,value){
-     //alert(key);
-    });
-
-})
+	    //open customize check list 
+	    $(".customize-column").on('click', function(){
+	    	$(".checkbox-cont").show();
+	    });
+	    $(".close-btn").on('click', function(){
+	    	$(".checkbox-cont").hide();
+	    });
 
 
+	    //Column filter
+		$('[data-id]').hide();
+		$('[data-id]').on("click", function(){	
+			var checkboxId = $(this).attr('data-id');        		
+			if($(this).is(":checked")) {				
+				$(this).parent().removeClass('disabled');
+				$('[data-id="' + checkboxId + '"]').show();   
+		    } else {	    	
+		    	$(this).parent().addClass('disabled');
+		    	$('[data-id="' + checkboxId + '"]').hide();   
+		    }	   	
+		});
+		$('[data-id]').slice(0,5).trigger('click');
+
+		// Slider for customize columns.
+		$('.prev-icon').click(function(e) {      	
+		    $('.list-wrap').animate({scrollLeft: "-=177px"}, "slow");
+		});
+		$('.next-icon').click(function(e) {       	
+		    $('.list-wrap').animate({scrollLeft: "+=177px"}, "slow");
+		});
+
+	}
+}
+
+$(document).ready(function(){	
+  n42.init();  
+});
